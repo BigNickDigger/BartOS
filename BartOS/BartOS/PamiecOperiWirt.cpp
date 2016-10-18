1e0b9bbb08864b9e769ee8aa0d529c191410fdff
Kuba Kaczmarek <kubak3marek@hotmail.com>
2016-10-18 23:18:33 +0200

    we're in boys

---
+++ BartOS/BartOS/PamiecOperiWirt.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "PamiecOperiWirt.h"
+
+
+PamiecOperiWirt::PamiecOperiWirt()
+{
+}
+
+
+PamiecOperiWirt::~PamiecOperiWirt()
+{
+}
---
+++ BartOS/BartOS/PamiecOperiWirt.h
@@ -0,0 +1,8 @@
+#pragma once
+class PamiecOperiWirt
+{
+public:
+	PamiecOperiWirt();
+	~PamiecOperiWirt();
+};
+
