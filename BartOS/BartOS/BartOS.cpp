6e5f5415f1298f42156d9add125f2d8d5731610b
BigNickDigger <freaktime234@gmail.com>
2016-10-18 15:03:26 +0200

    pootis

--- BartOS/BartOS/BartOS.cpp
+++ BartOS/BartOS/BartOS.cpp
@@ -6,10 +6,11 @@ Krzysiu	= Komunikacja procesów
 Kuba	= Zarz¹dzanie pamiêci¹ opercyjn¹ i wirtualn¹
 Darek	= Zarz¹dzanie procesami(Tworzenie procesów)
 Marcin	= System plików
+Adam	= ASSembler interpreter
 
 System Windowso-niepodobny
 Wpisujcie tutaj propozycje nazw:
-
+BartOS
 
 
 
@@ -19,8 +20,9 @@ Wpisujcie tutaj propozycje nazw:
 */
 
 #include "stdafx.h"
-
-
+#include <iostream>
+using namespace std;
+const float SystemVer = 1.337;
 int main()
 {
     return 0;
