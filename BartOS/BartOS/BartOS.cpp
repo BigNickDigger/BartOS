1441e9f8006c613e33d41f53fbf022d2febe3d4f
BigNickDigger <freaktime234@gmail.com>
2016-10-18 15:01:40 +0200

    Pootis

--- BartOS/BartOS/BartOS.cpp
+++ BartOS/BartOS/BartOS.cpp
@@ -1,5 +1,22 @@
-// BartOS.cpp : Defines the entry point for the console application.
-//POOTIS
+/*
+Piotrek = Przyznawanie priorytetów procesom
+Olaw	= Mechanizmy synchronizacyjne
+Bartek	= Shell i programy ASSembler
+Krzysiu	= Komunikacja procesów
+Kuba	= Zarz¹dzanie pamiêci¹ opercyjn¹ i wirtualn¹
+Darek	= Zarz¹dzanie procesami(Tworzenie procesów)
+Marcin	= System plików
+
+System Windowso-niepodobny
+Wpisujcie tutaj propozycje nazw:
+
+
+
+
+
+
+
+*/
 
 #include "stdafx.h"
 
