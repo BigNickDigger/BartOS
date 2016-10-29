c8fca6fd458e5cedb4438a29bee30ed6ab6d50db
Kuba Kaczmarek <kubak3marek@hotmail.com>
2016-10-29 17:00:02 +0200

    k

--- BartOS/BartOS/PamiecOperiWirt.cpp
+++ BartOS/BartOS/PamiecOperiWirt.cpp
@@ -4,13 +4,35 @@
 
 PamiecOperiWirt::PamiecOperiWirt()
 {
-	
+	for (int i = 0; i < 255; i++)//utworzenie pamieci wirtualnej, 256 pustych stron
+	{
+		singlepage = new page;
+		PWirt.push_back(*singlepage);
+	}
+	for (int i = 0; i < 8; i++)
+	{
+		singlepage = new page;
+		POper.push_back(*singlepage);//utworzenie pamieci operacyjnej, 8 pustych ramek
+	}
 }
 
 
 PamiecOperiWirt::~PamiecOperiWirt()
 {
-
+	
 }
 
+int PamiecOperiWirt::WhichPage(short int x)
+{
+	return (x / 16);
+}
 
+int PamiecOperiWirt::WhatOffset(short int x)
+{
+	return (x % 16);
+}
+
+void PamiecOperiWirt::AktualizujPamiecWirtualna()
+{
+	
+}
--- BartOS/BartOS/PamiecOperiWirt.h
+++ BartOS/BartOS/PamiecOperiWirt.h
@@ -1,28 +1,35 @@
 #pragma once
 #include <iostream>
 #include <list>
-
-#define framesize = 16;
-#define framecount = 3;//operacyjna sklada sie z 3 ramek
-#define adreslength = 8;//2^8 = 256  /hex(FF)/  limit adresu dla stron, istnieje 256 stron na pamieci wirtualnej, beda one sciagane do ramek
-
+#include "PCB.h"
 using namespace std;
-typedef struct page {
-	int nr;
-	char data; //albo string???? jak bedzie char, to chyba bez sensu bedzie
-	bool valid = false; // true = jest w operacyjnej || false = nie ma w operacyjnej trzeba sciagnac z wirtualnej
+//proszê siê nie sugerowac moimi rozwi¹zaniami, tu jeszcze wszystko mo¿e siê zmieniæ
+
+const int framesize = 16;//rozmiar ramki/strony
+const int framecount = 8;//operacyjna sklada sie z 8 ramek, czyli 8*16 bajtów = 128 bajtów pamiêci
+const int adreslength = 8;//2^8 = 256  /hex(FF)/  limit adresu dla stron ( (dec)1111_1111 = 255), istnieje 256 stron na pamieci wirtualnej, beda one sciagane do ramek
+
+struct page {
+	unsigned short nr;
+	string data; //Nie wiem jaki typ tu daæ. Jak zostanie string, to bedzie to string na 14charów/bajtów, bo 2 bajty id¹ na NR, a ca³y page=16 bajtów
 };
 
 class PamiecOperiWirt
 {
 public:
-	
-	list <page> POper;
-	list <page> PWirt;
-
 	PamiecOperiWirt();
 	~PamiecOperiWirt();
 
+	int AktualnyRozmiarOP;//zlicza ile razy u¿yta zostanie operacja push_back
+	int AktualnyRozmiarWIRT;//zlicza ile razy u¿yta zostanie operacja push_back
+	page *singlepage;
+	list <page> POper;
+	list <page> PWirt;
+
+	int WhichPage(short int);//MMU do przeliczania adresu logicznego na fizyczny
+	int WhatOffset(short int);//MMU
+
+	void AktualizujPamiecWirtualna();//kontrowersyjna procedura, musze mieæ swój bank (pamiêc wirt.) z której bêdê przepisywa³ do operacyjnej pojedyncze bloki danych. Tym bankiem nie mo¿e byæ sam dysk (s³owa bartoszka) wiêc trzeba dokonywaæ jakiegoœ przepisywania dysku do mojego banku, tym bêdzie siê zajmowaæ ta procedura
 
 };
 
