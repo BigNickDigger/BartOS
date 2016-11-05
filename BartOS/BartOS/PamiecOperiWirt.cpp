176b04c5134a4edf2775eed5e4e8def5694f5dd4
Kuba Kaczmarek <kubak3marek@hotmail.com>
2016-11-05 20:49:17 +0100

    kkk

--- BartOS/BartOS/PamiecOperiWirt.cpp
+++ BartOS/BartOS/PamiecOperiWirt.cpp
@@ -4,54 +4,48 @@
 
 PamiecOperiWirt::PamiecOperiWirt()
 {
-	for (int i = 0; i < framecount; i++)
-	{
-		singlepage = new page;
-		singlepage->data.reserve(14);
-		singlepage->nr = i;//numeruje ramki od 0 do 7
-		POper.push_back(*singlepage);//utworzenie pamieci operacyjnej, 8 pustych ramek, po iteratorze mo¿na wpisywaæ
-	}
-	LicznikDoStron = 0;
+	POnr = 0;
+	PWnr = 0;
 }
 
 
 PamiecOperiWirt::~PamiecOperiWirt()
 {
-	delete singlepage;
+	//delete singlepage;
 }
 
-//int PamiecOperiWirt::WhichPage(short int x)
-//{
-//	return (x / 16);
-//}
-//
-//int PamiecOperiWirt::WhatOffset(short int x)
-//{
-//	return (x % 16);
-//}
 
-void PamiecOperiWirt::StworzPamiecWirtualna()
-{
+//void PamiecOperiWirt::StworzPamiecWirtualna()
+//{
 	//tworzy strone, wpisuje 14 charow i pushuje do listy stron. Powtarza az do wyczerpania charow na dysku
-}
+//}
 
 void PamiecOperiWirt::DeleteProcess(PCB blok)
 {
-	int i = 0;
-	for (iter = POper.begin(); iter != POper.end(); i++)
-	{
-		
-		if (blok.pages[i].Valid == true)//gdy iter najedzie na ramke dla ktorej w tablicy stronic bit valid jest rowny TRUE
-		{
-			POper.erase(iter++);
-			singlepage = new page;
-			singlepage->nr = i;
-			POper.insert(iter,*singlepage);
-		}
-		else
-		{
-			iter++;
-		}
-	}
 	
+	
+}
+
+void PamiecOperiWirt::AllocateMemory(unsigned short x)
+{
+	
+}
+
+
+
+
+void PamiecOperiWirt::ShowOPMemory()
+{
+	for (int i = 0; i < 8; i++)
+	{
+		this->POper[i].ShowFrame();
+	}
+}
+void PamiecOperiWirt::ShowVirtualMemory()
+{
+	for (iter = PWirt.begin(); iter != PWirt.end(); ++iter)
+	{
+		iter->ShowFrame();
+	}
+	iter = PWirt.begin();
 }
