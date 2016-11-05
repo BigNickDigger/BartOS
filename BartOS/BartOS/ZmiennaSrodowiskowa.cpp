2b66210aa9ceed9bd3bf61e5909f0dab6db4d0a9
BarKar96 <karlowski.bartek@gmail.com>
2016-11-05 20:47:53 +0100

    troszke shella

---
+++ BartOS/BartOS/ZmiennaSrodowiskowa.cpp
@@ -0,0 +1,16 @@
+#include "stdafx.h"
+#include "ZmiennaSrodowiskowa.h"
+
+
+ZmiennaSrodowiskowa::ZmiennaSrodowiskowa(std::string nazwa, std::string sciezka)
+{
+	this->nazwa = nazwa;
+	this->sciezka = sciezka;
+
+
+}
+
+
+ZmiennaSrodowiskowa::~ZmiennaSrodowiskowa()
+{
+}
---
+++ BartOS/BartOS/ZmiennaSrodowiskowa.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <string>
+class ZmiennaSrodowiskowa
+{
+private:
+	std::string nazwa;
+	std::string sciezka;
+public:
+	
+	ZmiennaSrodowiskowa(std::string nazwa, std::string sciezka);
+	~ZmiennaSrodowiskowa();
+
+};
+
