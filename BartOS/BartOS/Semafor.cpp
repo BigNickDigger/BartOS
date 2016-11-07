4e0a71127f5d38dea098304a2efa097a9d1ec614
Olaf <olafbergmann040496@gmail.com>
2016-11-07 18:55:32 +0100

    komit dla Klapy

--- BartOS/BartOS/Semafor.cpp
+++ BartOS/BartOS/Semafor.cpp
@@ -4,6 +4,7 @@
 
 Semafor::Semafor()
 {
+	svalue = 1;
 }
 
 
@@ -13,10 +14,23 @@ Semafor::~Semafor()
 
 void Semafor::wait()
 {
+	svalue--;
+	if (svalue < 0)
+	{
+		//dodanie do kolejki FIFO procesów oczekuj¹cych
+		//wstrzymanie procesu block();
+	}
 
 }
 
 void Semafor::signal()
 {
+	svalue++;
+	if (svalue <= 0)
+	{
+		//bierzemy proces z kolejki FIFO
+		//wznawiamy proces wakeup();
+
+	}
 
 }
--- BartOS/BartOS/Zamek.cpp
+++ BartOS/BartOS/Zamek.cpp
@@ -4,6 +4,7 @@
 
 Zamek::Zamek()
 {
+	zvalue = 0;
 }
 
 
@@ -13,12 +14,27 @@ Zamek::~Zamek()
 
 void Zamek::lock()
 {
+	if (zvalue == 0)
+	{
+		zvalue = 1;
+	}
+	else
+	{
+	//ustawiam w¹tek na oczekuj¹cy i umieszczam w kolejce
+	}
 
 }
 
 void Zamek::unlock()
 {
-
+	if(/*kolejka procesów pusta*/)
+	{
+	zvalue = 0;
+	}
+	else
+	{
+		//biorê w¹tek z kolejki procesów oczekuj¹cych i zmieniam jego stna na gotowy
+	}
 }
 
 
