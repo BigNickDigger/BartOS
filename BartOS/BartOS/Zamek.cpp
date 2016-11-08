1743f3789bebee1d7268003259a56f8023ad9b0e
Olaf <olafbergmann040496@gmail.com>
2016-11-08 20:57:11 +0100

    zmiany zmiany

--- BartOS/BartOS/Zamek.cpp
+++ BartOS/BartOS/Zamek.cpp
@@ -11,30 +11,36 @@ Zamek::~Zamek()
 {
 }
 
-void Zamek::lock()
+void Zamek::lock(int ID_procesu)
 {
+	
 	if (zvalue == 0)
 	{
+		this->ID_procesu = ID_procesu;
 		zvalue = 1;
 	}
 	else
 	{
-		KPZ.push(/*Identyfikator w¹tku PID*/0); // umieszam w¹tek w kolejsce 
+		KPZ.push(ID_procesu); // umieszam w¹tek w kolejsce 
 		//KPZ.back(); i zmieniam  jego stan na oczekuj¹cy
 	}
 }
 
-void Zamek::unlock()
+void Zamek::unlock(int ID_procesu)
 {
-	if (KPZ.empty())
+	if (this->ID_procesu != ID_procesu)
 	{
-		zvalue = 0;
-	}
-	else
+		if (KPZ.empty())
+		{
+			zvalue = 0;
+		}
+		else
 
-	{	//KPZ.front(); // zmieniam stan w¹tku na gotowy
-		KPZ.pop();//i biorê go z kolejki w¹tków oczekuj¹cych
-	
+		{	//KPZ.front(); // zmieniam stan w¹tku na gotowy
+			KPZ.pop();//i biorê go z kolejki w¹tków oczekuj¹cych
+			this->ID_procesu = ID_procesu;
+
+		}
 	}
 }
 
--- BartOS/BartOS/Zamek.h
+++ BartOS/BartOS/Zamek.h
@@ -6,8 +6,8 @@ class Zamek
 private:
 	bool zvalue;
 public:
-	void lock();
-	void unlock();
+	void lock(int ID_procesu);
+	void unlock(int ID_procesu);
 	std::queue < int > KPZ; //kolejka w¹tków oczekuj¹cych
 	int ID_procesu;
 	Zamek();
