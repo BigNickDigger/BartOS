0542416b1416d67db15cdc4458ec4cfa5eec93ca
Olaf <olafbergmann040496@gmail.com>
2016-11-08 21:03:57 +0100

    zmiany zmiany zmiany

--- BartOS/BartOS/Zamek.cpp
+++ BartOS/BartOS/Zamek.cpp
@@ -22,7 +22,7 @@ void Zamek::lock(int ID_procesu)
 	else
 	{
 		KPZ.push(ID_procesu); // umieszam w¹tek w kolejsce 
-		//KPZ.back(); i zmieniam  jego stan na oczekuj¹cy
+		//KPZ.back(); i zmieniam  jego stan na oczekuj¹cy (nie mam jeszcze funkcji do tego czekam a¿ ktoœ zrobi, tylko kto?????)
 	}
 }
 
@@ -36,7 +36,7 @@ void Zamek::unlock(int ID_procesu)
 		}
 		else
 
-		{	//KPZ.front(); // zmieniam stan w¹tku na gotowy
+		{	//KPZ.front(); // zmieniam stan w¹tku na gotowy (nie mam jeszcze funkcji do tego czekam a¿ ktoœ zrobi, tylko kto?????)
 			KPZ.pop();//i biorê go z kolejki w¹tków oczekuj¹cych
 			this->ID_procesu = ID_procesu;
 
